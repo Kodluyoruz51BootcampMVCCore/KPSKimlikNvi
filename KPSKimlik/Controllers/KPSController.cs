b3ed4a0f9937c56005d7f58d140011538f2021f4
Hüseyin Karacif <hsynkrcf@gmail.com>
2020-07-05 22:23:20 +0300

    Add project files.

---
+++ KPSKimlik/Controllers/KPSController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KPSKimlik.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ServiceReference1;
+
+namespace KPSKimlik.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KPSController : ControllerBase
+    {
+        [HttpGet]
+        [Route("kimlikdogrula")]
+        public async Task<IActionResult> Get(string tcKimlikNo,string ad,string soyad,string dogumYili)    // GET isteği çalışıyor
+        {
+            KPSPublicSoapClient servis = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            var sonuc = await servis.TCKimlikNoDogrulaAsync(long.Parse(tcKimlikNo), ad, soyad, int.Parse(dogumYili));
+            return Ok(JsonConvert.SerializeObject(sonuc.Body.TCKimlikNoDogrulaResult));
+        }
+
+        [HttpPost]
+        [Route("kimlikdogrula")]
+        public async Task<IActionResult> Post([FromBody]Vatandas vatandas)       // POST bad requeste düşüyor
+        {
+            KPSPublicSoapClient servis = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            var sonuc = await servis.TCKimlikNoDogrulaAsync(vatandas.TCKimlikNo, vatandas.Ad, vatandas.Soyad, vatandas.DogumYili);
+            return Ok(JsonConvert.SerializeObject(sonuc.Body.TCKimlikNoDogrulaResult));
+        }
+    }
+}
---
+++ KPSKimlik/Models/Vatandas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KPSKimlik.Models
+{
+    public class Vatandas
+    {
+        public long TCKimlikNo { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+    }
+}
